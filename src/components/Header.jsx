4672d316fc60ed0d0c52b7e68e29c567afe9db23
Jamienorthman <mikoann88@gmail.com>
2024-01-10 18:35:11 +0100

    all img paths corrected

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     return (
         <div className="header">
             <img className="banner-img" 
-            src="./src/icons/restaurant-menu.png"
+            src="/icons/restaurant-menu.png"
             alt="orange-red, yellow, and purple logo for juice shop"
             />
             <h3 className="title">SunriseSunset</h3>
